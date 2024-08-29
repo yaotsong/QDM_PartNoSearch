19b46eb6db305b1f845e99b5577344c0b1024ff9
yaotsong <sam502131307@gmail.com>
2024-08-29 16:42:22 +0800

    調整SSL憑證一律通過

--- QDM_PartNoSearch/Program.cs
+++ QDM_PartNoSearch/Program.cs
@@ -1,37 +1,44 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using QDM_PartNoSearch.Models;
 using QDM_PartNoSearch.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 全局禁用證書驗證（僅在開發環境中使用）
+ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+
+// 配置 HttpClient
+builder.Services.AddHttpClient("NoCertValidationClient")
+    .ConfigurePrimaryHttpMessageHandler(() =>
+    {
+        return new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (message, certificate, chain, sslPolicyErrors) => true
+        };
+    });
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<Flavor2Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBContext")));
-// 添加服務到容器裡
-builder.Services.AddHttpClient();
 builder.Services.AddMemoryCache(); // 添加內存緩存服務
-
-// 註冊 TokenRefreshService
 builder.Services.AddHostedService<TokenRefreshService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=HomePage}/{id?}");
