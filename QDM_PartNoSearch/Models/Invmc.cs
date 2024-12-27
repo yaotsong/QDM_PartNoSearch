a3c041b8f0891407925f9c6a7880264d7b055584
yaotsong <sam502131307@gmail.com>
2024-12-27 14:47:17 +0800

    新增Class備註

--- QDM_PartNoSearch/Models/Invmc.cs
+++ QDM_PartNoSearch/Models/Invmc.cs
@@ -2,9 +2,11 @@
 {
     public class Invmc
     {
+        //品號
         public string Mc001 { get; set; } = null!;
-
+        //庫別
         public string? Mc002 { get; set; }
+        //庫存數量
         public decimal Mc007 { get; set; }
     }
 }
--- QDM_PartNoSearch/Models/Invmh.cs
+++ QDM_PartNoSearch/Models/Invmh.cs
@@ -5,8 +5,9 @@ namespace QDM_PartNoSearch.Models;
 
 public partial class Invmh
 {
+    //商品品號
     public string? MH001 { get; set; }
-
+    //商品條碼
     public string? MH002 { get; set; }
 
 }
